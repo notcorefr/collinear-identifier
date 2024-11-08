e1fa8745720362fe383babf2fca4a0dd7eab689b
anuj-weebjs <weeb.js@yahoo.com>
2024-11-08 15:50:05 +0530

    inital commit

---
+++ main.ts
@@ -0,0 +1,79 @@
+const prompt = require('prompt-sync')();
+
+class Point {
+    readonly name: string = '';
+    readonly cordinates: { x: number, y: number } = { x: 0, y: 0 };
+
+
+    constructor(name: string, x: number, y: number) {
+        this.name = name;
+        this.cordinates.x = x;
+        this.cordinates.y = y;
+    }
+
+}
+
+let d = (a: Point, b: Point): number => {
+    let distance: number;
+    distance = Math.sqrt(Math.pow(b.cordinates.x - a.cordinates.x, 2) + Math.pow(b.cordinates.y - a.cordinates.y, 2))
+    console.log(`d(${a.name},${b.name}) = ${distance}`);
+    return distance;
+}
+
+let areColinear = (a: Point, b: Point, c: Point): boolean => {
+    // ab bc ac 
+    let ab = d(a, b);
+    let bc = d(b, c);
+    let ac = d(a, c);
+
+    if (ab > bc && ab > ac) {
+        if (ab == bc + ac) return true;
+    } else if (bc > ab && bc > ac) {
+        if (bc == ab + ac) return true;
+    } else if (ac > ab && ac > bc) {
+        if (ac == ab + ac) return true;
+    }
+
+    return false;
+}
+
+let horizontalLine = (): void => {
+    console.log('---------------')
+}
+
+
+horizontalLine();
+console.log(`To Check Whether The points are colinear or not, Enter the Co-ordinates for those 3 Points,\nsuppose these 3 points are A, B, C`)
+
+
+let points: Point[] = [];
+
+for (let i = 0; i < 3; i++) {
+    let name = String.fromCharCode(65 + i);
+    horizontalLine();
+    console.log(`${name}(x,y)`);
+    let x = parseInt(prompt('x: '));
+    let y = parseInt(prompt('y: '));
+    if (isNaN(x) || isNaN(y)) {
+        console.error("You Must Provide Integers as co-ordinats!");
+        process.exit(0);
+    }
+
+    console.log(`${name}(${x},${y})`);
+
+    points.push(new Point(name, x, y));
+
+}
+
+
+horizontalLine();
+
+const _areColinear = areColinear(points[0], points[1], points[2]);
+console.log(_areColinear)
+
+horizontalLine();
+
+console.log(`Results\n${points[0].name}(${points[0].cordinates.x},${points[0].cordinates.y}), ${points[1].name}(${points[1].cordinates.x},${points[1].cordinates.y}), ${points[2].name}(${points[2].cordinates.x},${points[2].cordinates.y})\nare${_areColinear ? ' ' : ' not '}Co-Linear Points`)
+
+
+export { }
